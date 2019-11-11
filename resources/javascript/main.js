25f4b079f7438a72412815a2cbcd8f5c7c1844b2
Jing <jinjingcs@gmail.com>
2019-11-11 22:29:48 +0100

    no cover letter

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,8 +1,41 @@
+
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "25rem";
+    document.getElementById("mySidenav").style.width = "20rem";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+let overlay = document.getElementsByClassName("overlay");
+let projectImg = document.querySelectorAll(".project-img");
+
+for (let i=0; i< overlay.length; i++) {
+    
+    overlay[i].onmouseover = function(){ 
+    projectImg[i].filter = "blur(5px)";
+    projectImg[i].opacity= 0.8
+    };
+}
+
+console.log(overlay)
+console.log(projectImg)
+
+function openLetter() {
+  document.getElementById("Cover-Letter").style.display = "flex";
+  document.getElementById("CV").style.display = "none";
+  document.getElementsByClassName("tablinks")[0].style.backgroundColor = "#faeaea";
+  document.getElementsByClassName("tablinks")[1].style.backgroundColor = "initial";
+  
+ }
+
+ function openCV() {
+  document.getElementById("Cover-Letter").style.display = "none";
+  document.getElementById("CV").style.display = "flex";
+  document.getElementById("defaultOpen").style.backgroundColor = "initial";
+  document.getElementsByClassName("tablinks")[1].style.backgroundColor = "#faeaea";
+
+ } 
+
+    
